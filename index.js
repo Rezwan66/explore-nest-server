e1c228542fa9776bdf3907f5f977242e2e5adaf1
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-27 21:04:23 +0600

    created delete api to delete an item by id from wishlist

--- index.js
+++ index.js
@@ -195,6 +195,12 @@ async function run() {
             const result = await wishlistCollection.insertOne(wishlistItem);
             res.send(result);
         })
+        app.delete('/wishlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await wishlistCollection.deleteOne(query);
+            res.send(result);
+        })
 
     } finally {
         // Ensures that the client will close when you finish/error
